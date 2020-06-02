3d387199558733bb9423dc474fa67eefe971e1e6
Rizqi Kartika Safitri <rkartikasafitri@gmail.om>
2020-06-02 12:36:51 +0700

    Update App.js

--- project-dashboard/src/App.js
+++ project-dashboard/src/App.js
@@ -5,6 +5,9 @@ import ReadUser from './models/read.user'
 import Footer from './assets/footer'
 
 export default class App extends Component {
+  /**
+   * @description method untuk menampilkan halaman dashboard yang berisi kelas komponen header, menu, content, dan footer 
+   */
   render() {
     return (
       <div>
