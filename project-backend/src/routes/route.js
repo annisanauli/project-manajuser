f3d23d02c979ad47e6d15530d3cb3c53987d3133
rizqikartika <43631445+rizqikartika@users.noreply.github.com>
2020-06-04 15:12:03 +0700

    Update route.js

--- project-backend/src/routes/route.js
+++ project-backend/src/routes/route.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
-
 router.get('/list', UserController.list);
 router.post('/create', UserController.create);
 
