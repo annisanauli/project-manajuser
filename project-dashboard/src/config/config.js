3fc6643388d7cdef4e443a7c30825620071b772e
Rizqi Kartika Safitri <rkartikasafitri@gmail.om>
2020-06-11 10:37:24 +0700

    Update config.js

--- project-dashboard/src/config/config.js
+++ project-dashboard/src/config/config.js
@@ -1,19 +1,3 @@
-// import React, { Component } from 'react';
-
-
-// class config extends Component{
-   
-//     url() {
-//         const baseurl = "https://localhost:3000"
-
-        
-//       }
-// }
-// const b = new B();
-// export default config;
-
-
 export default({
     baseUrl: 'http://localhost:3000'
-   
 });
