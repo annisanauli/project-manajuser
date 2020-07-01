e027e7475a5be9278864b23c3bf34ad38299efa9
Rizqi Kartika Safitri <rkartikasafitri@gmail.om>
2020-07-01 22:39:26 +0700

    Update index.test.js

---
+++ project-dashboard/src/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "../App";
+import '../index.js'
+
+jest.mock('react-dom', ()=> ({render: jest.fn()}))
+
+
+it('renders without crashing', () => {
+
+  const div = document.createElement('div');
+  ReactDOM.render(<App/>, div);
+  global.document.getElementById = (id) => id ==='root' && div
+  expect(ReactDOM.render).toHaveBeenCalledWith(<App />, div)
+  expect(ReactDOM).toMatchSnapshot();
+});
+
