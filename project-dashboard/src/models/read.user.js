9c03c45065abc545cfb2b7d398017ba840ccbd65
annisanauli <annisanauli@gmail.com>
2020-07-30 11:25:19 +0700

    Update documentation file read.user.js

--- project-dashboard/src/models/read.user.js
+++ project-dashboard/src/models/read.user.js
@@ -6,10 +6,15 @@ import Config from '../config/config';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
 import 'sweetalert2/src/sweetalert2.scss';
 
+/**
+ * @description kelas readuser merupakan kelas untuk menampilkan tabel berisi data user yang tersimpan didatabase
+ */
 class readUser extends Component {
   /**
-   * @description konstruktor kelas konten
-   * @param {*} props untuk menampung daftar user yang terdapat pada database
+   * @description konstruktor kelas readuser untuk menginisialisasi atribut yang dibutuhkan pada kelas readuser
+   * @param {*} props properti untuk kelas form
+   * Terdapat state berisi listUser.
+   * listUser untuk menampung data yang terdapat pada database
    */
   constructor(props){
     super(props);
@@ -25,7 +30,8 @@ class readUser extends Component {
   }
 
   /**
-   * @description method untuk memanggil dan memanipulasi data agar data dapat ditampil sesuai dengan paging short, limit entry
+   *@description fungsi untuk memanggil dan memanipulasi data agar data dapat ditampil 
+   * sesuai dengan paging short, limit entry
    */
   componentDidMount(){
     this.loadUser()
@@ -37,7 +43,9 @@ class readUser extends Component {
   }
 
   /**
-   * @description method untuk memanggil dan memanipulasi data dari database ke server
+   * loadUser
+   * 
+   * @description fungsi untuk memanggil dan memanipulasi data dari database ke server
    */
   loadUser(){
     const url = Config.baseUrl+"/users"
@@ -56,9 +64,6 @@ class readUser extends Component {
     })
   } 
 
-    /**
-     * @description method untuk manampilkan tabel daftar user pada halaman dashboard
-     */
     render() {
       return (
         <div>
@@ -113,7 +118,9 @@ class readUser extends Component {
     }
 
     /**
-     * @description method untuk memanggil sesuai dengan permintaan kedalam tabel
+     * loadFillData
+     * 
+     * @description fungsi untuk memanggil data sesuai dengan kolom tabel
      */
     loadFillData(){
       return this.state.listUser.map((data)=>{
@@ -147,6 +154,14 @@ class readUser extends Component {
         )
       })
     }
+
+    /**
+     * onEdit
+     * 
+     * @description fungsi untuk mengambil id sesuai dengan kolom yang dipilih 
+     * dan mengirimnya ke fungsi getData yang pada file edit.user
+     * @param {Integer} id variabel untuk menampung id user
+     */
     onEdit(id){ 
       // this.setState({
       //   activeId:id
@@ -156,6 +171,14 @@ class readUser extends Component {
       this.child.current.getData(id);
     }  
 
+    /**
+     * onDelete
+     * 
+     * @description fungsi untuk memanggil id sesuai dengan kolom yang dipilih, 
+     * dan akan diberi pilihan "apakah yakin untuk dihapus", jika iya maka akan memanggil fungsi sendDelete
+     * jika tidak, maka akan kembali ke dashboard, dan mendapat notif bahwa tidak jadi menghapus kolom
+     * @param {Integer} id variabel untuk menampung id user 
+     */
     onDelete(id){
       Swal.fire({
         title: 'Delete User',
@@ -179,12 +202,18 @@ class readUser extends Component {
       })
     }
 
+  /**
+   * sendDelete
+   * 
+   * @description fungsi untuk menghapus data sesuai dengan id yang didapatkan di fungsi onDelete
+   * @param {Integer} userId 
+   */  
   sendDelete(userId)
   {
     // url backend
     const url = Config.baseUrl+"/users/"+userId    // parameter data post
     // network
-    axios.delete(url,{id:userId})
+    return axios.delete(url,{id:userId})
     .then(response =>{
       console.log(response)
       if (response.data.success) {
